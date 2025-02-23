dc16fde4e8ff833f4f6991d97017c6e5e01534ae
Leandro Figueiredo <leeeandrofigueiredo@gmail.com>
2025-02-23 17:47:32 -0300

    menu hamburguer finalizado

--- Amiros-Turismo-main/bs-travelic/Recursos/js/mobile-navbar.js
+++ Amiros-Turismo-main/bs-travelic/Recursos/js/mobile-navbar.js
@@ -4,10 +4,26 @@ class MobileNavbar {
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index)=>{
+            link.style.animation
+            ? (link.style.animation = " ")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+        });
+    }
+
+    handleClick(){
+        
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", () => console.log("Hey"));
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     init() {
@@ -22,4 +38,5 @@ class MobileNavbar {
         ".mobile-menu",
         ".nav-list",
         ".nav-list li",
-    ); mobileNavbar.init();
+    );
+     mobileNavbar.init();
